2d81d9f9888f90db17785b3fe3ffc79d82ab64eb
money1031 <hexiaotiansky@gmail.com>
2026-01-12 08:46:09 +0800

    Site updated: 2026-01-12 08:46:08

--- yxjs/yuxing.js
+++ yxjs/yuxing.js
@@ -1,25 +1,30 @@
-$(function () {
+<script>
+(function () {
+  const poiName = "元明机械";
 
-  // tab 切换
-  $('.tab-btn').click(function () {
-    const key = $(this).data('tab');
+  document.getElementById("navBtn").onclick = function () {
+    const ua = navigator.userAgent.toLowerCase();
 
-    $('.tab-btn').removeClass('active');
-    $(this).addClass('active');
+    // 百度地图 App（只用名称，避免坐标偏移）
+    const appUrl =
+      `baidumap://map/direction?destination=${encodeURIComponent(poiName)}&mode=driving`;
 
-    $('.tab-content').removeClass('active');
-    $('#tab-' + key).addClass('active');
-  });
+    // 百度地图网页版（兜底，不 404）
+    const webUrl =
+      `https://map.baidu.com/dir/?destination=${encodeURIComponent(poiName)}`;
 
-  // 图片放大
-  $('.zoom-img, .param-btn').click(function () {
-    const img = $(this).data('img') || $(this).attr('src');
-    $('#popupImg').attr('src', img);
-    $('#imgPopup').fadeIn();
-  });
+    if (/iphone|ipad|ipod|android/.test(ua)) {
+      // 尝试唤起百度地图 App
+      window.location.href = appUrl;
 
-  $('.img-popup .close, .img-popup').click(function () {
-    $('#imgPopup').fadeOut();
-  });
-
-});
+      // App 未安装兜底
+      setTimeout(() => {
+        window.open(webUrl, "_blank");
+      }, 2000);
+    } else {
+      // PC
+      window.open(webUrl, "_blank");
+    }
+  };
+})();
+</script>
